258bdf1d40f8a156537440af3d527645250a5373
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-04-23 18:49:36 -0500

    BugFix manejo de Queries

--- src/main/java/com/dev2win/iniciativas/data/users/UserRepository.java
+++ src/main/java/com/dev2win/iniciativas/data/users/UserRepository.java
@@ -5,16 +5,17 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String userName);
 
-    @Query("SELECT * FROM sys.user WHERE profile=:profile")
-    List<User> findByProfile(String profile);
+    @Query("SELECT u FROM User u WHERE u.profile=:profile")
+    List<User> findByProfile(@Param("profile") String profile);
 
-    @Query("SELECT * FROM sys.user WHERE role=:role")
-    List<User> findByRole(String role);
+    @Query("SELECT u FROM User u WHERE u.role=:role")
+    List<User> findByRole(@Param("role") String role);
 
 }
