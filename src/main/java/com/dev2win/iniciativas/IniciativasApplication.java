2cd6624c20461d9fd7b3123c6824dd795ab4baba
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-04-27 08:35:30 -0500

    Ignoren esto, esta en proceso

--- src/main/java/com/dev2win/iniciativas/IniciativasApplication.java
+++ src/main/java/com/dev2win/iniciativas/IniciativasApplication.java
@@ -21,20 +21,31 @@ public class IniciativasApplication {
 
     @Autowired
     UserService userService;
+    InitiativeService initiativeService;
 
     public static void main(String[] args) {
         SpringApplication.run(IniciativasApplication.class, args);
     }
 
-    /*
-     * @Bean
-     * public CommandLineRunner run() throws Exception {
-     * return (args) -> {
-     * 
-     * System.out.println(userService.getUserByProfile("Estudiante"));
-     * };
-     * }
-     */
+    
+    @Bean
+    public CommandLineRunner run() throws Exception {
+        return (args) -> {
+            System.out.println("Deleting users and initiatives on DB...\n");
+            //initiativeService.getAllInitiatives().forEach(initiative -> initiativeService.deleteInitiative(initiative.getInitiativeId()));
+            //userService.getAllUsers().forEach(user -> userService.deleteUser(user.getUserId()));
+
+            // System.out.println(userService.getUserByProfile("Estudiante"));
+            User user = new User("prueba", "contrasena", Role.Administrador, "desarrollo", Profile.Estudiante, "prueba@mail.escuelaing.edu.co");
+            Initiative initiativeOne = new Initiative("description", "status", null, null, null, user);
+            Initiative initiativeTwo = new Initiative("description", "revision", null, null, null, user);
+        
+            user.getIdeas().add(initiativeOne);
+            user.getIdeas().add(initiativeTwo);
+            userService.addUser(user);
+        };
+    }
+     
 
     @Bean
     ServletRegistrationBean jsfServletRegistration(ServletContext servletContext) {
@@ -47,9 +58,6 @@ public class IniciativasApplication {
         srb.setUrlMappings(Arrays.asList("*.xhtml"));
         srb.setLoadOnStartup(1);
 
-        // adduser
-        User user = new User("prueba", "contrasena", Role.Administrador, "desarrollo", Profile.Estudiante, "prueba@mail.escuelaing.edu.co");
-        userService.addUser(user);
         return srb;
     }
 
