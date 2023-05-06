5154dad2eeb422d20d2846c7148d4fd3a4ba8787
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-05-06 08:29:15 -0500

    Filtor por estado y eliminación imports sobrantes

--- src/main/java/com/dev2win/iniciativas/data/ideas/InitiativeRepository.java
+++ src/main/java/com/dev2win/iniciativas/data/ideas/InitiativeRepository.java
@@ -1,11 +1,7 @@
 package com.dev2win.iniciativas.data.ideas;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface InitiativeRepository extends JpaRepository<Initiative, Long> {
