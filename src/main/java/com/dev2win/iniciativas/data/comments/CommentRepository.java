83a51a57aecea0cd352e0ad537876c8dbcf72de6
Angie Mojica <nataliamojica90@gmail.com>
2023-05-12 00:26:02 -0500

    Consulta comentarios por iniciativa

--- src/main/java/com/dev2win/iniciativas/data/comments/CommentRepository.java
+++ src/main/java/com/dev2win/iniciativas/data/comments/CommentRepository.java
@@ -3,10 +3,14 @@ package com.dev2win.iniciativas.data.comments;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-  List<Comment> findByInitiative(Long initiative_id);
+//  List<Comment> findByInitiative(Long initiative_id);
+  @Query("SELECT c FROM Comment c WHERE c.initiative.initiativeId=:initiativeId")
+  List<Comment> findByInitiative(@Param("initiativeId") Long initiativeId);
 
 }
