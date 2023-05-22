8364c18a289c71b17f97214b2d01deb611e94819
ELS4NTA <daniel1sa_@hotmail.com>
2023-05-21 22:36:45 -0500

    Actualizados charts a ChartJS

--- src/main/java/com/dev2win/iniciativas/faces/ChartBean.java
+++ src/main/java/com/dev2win/iniciativas/faces/ChartBean.java
@@ -1,21 +1,25 @@
 package com.dev2win.iniciativas.faces;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.dev2win.iniciativas.data.ideas.State;
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 
-import org.primefaces.event.ItemSelectEvent;
-import org.primefaces.model.chart.Axis;
-import org.primefaces.model.chart.AxisType;
-import org.primefaces.model.chart.BarChartModel;
-import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.axes.cartesian.CartesianScaleTitle;
+import org.primefaces.model.charts.axes.cartesian.CartesianScales;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearAxes;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearTicks;
+import org.primefaces.model.charts.bar.BarChartModel;
+import org.primefaces.model.charts.bar.BarChartOptions;
+import org.primefaces.model.charts.optionconfig.title.Title;
+import org.primefaces.model.charts.bar.BarChartDataSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
-import com.dev2win.iniciativas.data.ideas.Area;
 import com.dev2win.iniciativas.data.ideas.InitiativeService;
 
 
@@ -44,34 +48,149 @@ public class ChartBean implements Serializable {
     
     public void createBarModel() {
         barModel = new BarChartModel();
-
-        ChartSeries series = new ChartSeries();
-        series.setLabel("Series 1");
-        series.set(State.OPEN, initiativeService.countByState("Open"));
-        series.set(State.CLOSED, initiativeService.countByState("Closed"));
-        series.set(State.APROVED, initiativeService.countByState("Aproved"));
-        series.set(State.REVIEW, initiativeService.countByState("Review"));
-        
-        barModel.addSeries(series);
+        ChartData data = new ChartData();
+        BarChartDataSet barDataSet = new BarChartDataSet();
+        barDataSet.setLabel("Initiatives vs State");
+        // Colors
+        List<String> bgColor = new ArrayList<>();
+        bgColor.add("rgb(255, 99, 132, 0.2)");
+        bgColor.add("rgb(255, 159, 64, 0.2)");
+        bgColor.add("rgb(255, 205, 86, 0.2)");
+        bgColor.add("rgb(75, 192, 192, 0.2)");
+        barDataSet.setBackgroundColor(bgColor);
+        List<String> borderColor = new ArrayList<>();
+        borderColor.add("rgb(255, 99, 132)");
+        borderColor.add("rgb(255, 159, 64)");
+        borderColor.add("rgb(255, 205, 86)");
+        borderColor.add("rgb(75, 192, 192)");
+        barDataSet.setBorderColor(borderColor);
+        barDataSet.setBorderWidth(1);
+        // Values
+        List<Number> values = new ArrayList<>();
+        values.add(initiativeService.countByState("Open"));
+        values.add(initiativeService.countByState("Closed"));
+        values.add(initiativeService.countByState("Aproved"));
+        values.add(initiativeService.countByState("Review"));
+        barDataSet.setData(values);
+        data.addChartDataSet(barDataSet);
+        // Labels
+        List<String> labels = new ArrayList<>();
+        labels.add("Open");
+        labels.add("Closed");
+        labels.add("Aproved");
+        labels.add("Review");
+        data.setLabels(labels);
+        // Options
+        BarChartOptions options = new BarChartOptions();
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Initiatives Vs State");
+        title.setFontSize(18);
+        title.setFontStyle("bold");
+        options.setTitle(title);
+        // Axes
+        CartesianScales cScales = new CartesianScales();
+        CartesianLinearAxes linearYAxes = new CartesianLinearAxes();
+        CartesianLinearAxes linearXAxes = new CartesianLinearAxes();
+        CartesianScaleTitle scaleTitleY = new CartesianScaleTitle();
+        CartesianScaleTitle scaleTitleX = new CartesianScaleTitle();
+        CartesianLinearTicks ticks = new CartesianLinearTicks();
+        ticks.setStepSize(1);
+        scaleTitleY.setDisplay(true);
+        scaleTitleY.setText("Number of initiatives");
+        scaleTitleY.setFontSize(15);
+        scaleTitleX.setDisplay(true);
+        scaleTitleX.setText("State");
+        scaleTitleX.setFontSize(15);
+        linearYAxes.setBeginAtZero(true);
+        linearYAxes.setScaleTitle(scaleTitleY);
+        linearYAxes.setTicks(ticks);
+        linearYAxes.setOffset(true);
+        linearXAxes.setScaleTitle(scaleTitleX);
+        linearXAxes.setOffset(true);
+        cScales.addYAxesData(linearYAxes);
+        cScales.addXAxesData(linearXAxes);
+        options.setScales(cScales);
+        options.setBarPercentage(1.0);
+        // Data & Configuration
+        barModel.setData(data);
+        barModel.setOptions(options);
     }
 
     public void createBarModel2() {
         barModel2 = new BarChartModel();
+        ChartData data = new ChartData();
+        BarChartDataSet barDataSet = new BarChartDataSet();
+        barDataSet.setLabel("Initiatives vs Area");
+        // Colors
+        List<String> bgColor = new ArrayList<>();
+        bgColor.add("rgb(255, 99, 132, 0.2)");
+        bgColor.add("rgb(255, 159, 64, 0.2)");
+        bgColor.add("rgb(255, 205, 86, 0.2)");
+        bgColor.add("rgb(75, 192, 192, 0.2)");
+        bgColor.add("rgb(54, 162, 235, 0.2)");
+        barDataSet.setBackgroundColor(bgColor);
+        List<String> borderColor = new ArrayList<>();
+        borderColor.add("rgb(255, 99, 132)");
+        borderColor.add("rgb(255, 159, 64)");
+        borderColor.add("rgb(255, 205, 86)");
+        borderColor.add("rgb(75, 192, 192)");
+        borderColor.add("rgb(54, 162, 235)");
+        barDataSet.setBorderColor(borderColor);
+        barDataSet.setBorderWidth(1);
+        // Values
+        List<Number> values = new ArrayList<>();
+        values.add(initiativeService.countByArea("Environment"));
+        values.add(initiativeService.countByArea("Articial Intelligence"));
+        values.add(initiativeService.countByArea("Undertaking"));
+        values.add(initiativeService.countByArea("Animal rights"));
+        values.add(initiativeService.countByArea("Healthy life"));
+        barDataSet.setData(values);
+        data.addChartDataSet(barDataSet);
+        // Labels
+        List<String> labels = new ArrayList<>();
+        labels.add("Environment");
+        labels.add("Articial Intelligence");
+        labels.add("Undertaking");
+        labels.add("Animal rights");
+        labels.add("Healthy life");
+        data.setLabels(labels);
+        // Options
+        BarChartOptions options = new BarChartOptions();
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Initiatives Vs Area");
+        title.setFontSize(18);
+        title.setFontStyle("bold");
+        options.setTitle(title);
+        // Axes
+        CartesianScales cScales = new CartesianScales();
+        CartesianLinearAxes linearYAxes = new CartesianLinearAxes();
+        CartesianLinearAxes linearXAxes = new CartesianLinearAxes();
+        CartesianScaleTitle scaleTitleY = new CartesianScaleTitle();
+        CartesianScaleTitle scaleTitleX = new CartesianScaleTitle();
+        CartesianLinearTicks ticks = new CartesianLinearTicks();
+        ticks.setStepSize(1);
+        scaleTitleY.setDisplay(true);
+        scaleTitleY.setText("Number of initiatives");
+        scaleTitleY.setFontSize(15);
+        scaleTitleX.setDisplay(true);
+        scaleTitleX.setText("Area");
+        scaleTitleX.setFontSize(15);
+        linearYAxes.setBeginAtZero(true);
+        linearYAxes.setScaleTitle(scaleTitleY);
+        linearYAxes.setTicks(ticks);
+        linearYAxes.setOffset(true);
+        linearXAxes.setScaleTitle(scaleTitleX);
 
-        ChartSeries series2 = new ChartSeries();
-        series2.setLabel("Series2");
-        series2.set(Area.ENVIRONMENT.getValue(), initiativeService.countByArea("Environment"));
-        series2.set(Area.IA.getValue(), initiativeService.countByArea("Articial Intelligence"));
-        series2.set(Area.UNDERTAKING.getValue(), initiativeService.countByArea("Undertaking"));
-        series2.set(Area.ANIMALS.getValue(), initiativeService.countByArea("Animal rights"));
-        series2.set(Area.HEALTHY.getValue(), initiativeService.countByArea("Healthy life"));
-        barModel2.addSeries(series2);
-        barModel2.setTitle("Number of initiatives by area");
-        barModel2.setAnimate(true);
-        Axis xAxis = barModel2.getAxis(AxisType.X);
-        Axis yAxis = barModel2.getAxis(AxisType.Y);
-        xAxis.setLabel("Initiative area");
-        yAxis.setLabel("Number of initiatives");
+        linearXAxes.setOffset(true);
+        cScales.addYAxesData(linearYAxes);
+        cScales.addXAxesData(linearXAxes);
+        options.setScales(cScales);
+        options.setBarPercentage(1.0);
+        // Data & Configuration
+        barModel2.setData(data);
+        barModel2.setOptions(options);
     }
     
     public BarChartModel getBarModel() {
