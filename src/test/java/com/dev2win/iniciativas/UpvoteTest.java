3c71c9cf4a09ab3537f5cf88bfeac4f81b8df88c
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-05-26 22:17:59 -0500

    Sincronización de Area a UpvoteTest

--- src/test/java/com/dev2win/iniciativas/UpvoteTest.java
+++ src/test/java/com/dev2win/iniciativas/UpvoteTest.java
@@ -2,8 +2,6 @@ package com.dev2win.iniciativas;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,12 +9,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.dev2win.iniciativas.data.ideas.Area;
 import com.dev2win.iniciativas.data.ideas.Initiative;
 import com.dev2win.iniciativas.data.ideas.State;
+import com.dev2win.iniciativas.data.likes.Upvote;
 import com.dev2win.iniciativas.data.users.Profile;
 import com.dev2win.iniciativas.data.users.Role;
 import com.dev2win.iniciativas.data.users.User;
-import com.dev2win.iniciativas.data.likes.Upvote;
 
 @RunWith(MockitoJUnitRunner.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -25,7 +24,7 @@ class UpvoteTest {
     @ValueSource(longs = { 5L, 17L, 8L, 10L })
     void shouldSetUpvoteId(Long input) {
         User user = new User("Equis", "abc", Role.PROPONENTE, "", Profile.ESTUDIANTE, "x@escuelaing.edu.co");
-        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user);
+        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user, Area.ANIMALS);
         Upvote upvote = new Upvote(initiative, user);
         upvote.setUpvoteId(input);
         assertEquals(input, upvote.getUpvoteId());
@@ -34,7 +33,7 @@ class UpvoteTest {
     @Test
     void shouldSetUser() {
         User user = new User("Equis", "abc", Role.PROPONENTE, "", Profile.ESTUDIANTE, "x@escuelaing.edu.co");
-        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user);
+        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user, Area.IA);
         Upvote upvote = new Upvote(initiative, user);
         User anotherUser = new User("Juan", "def", Role.PROPONENTE, "", Profile.ESTUDIANTE, "juan@mail.com");
         upvote.setUser(anotherUser);
@@ -44,7 +43,7 @@ class UpvoteTest {
     @Test
     void shouldGiveUser() {
         User user = new User("Equis", "abc", Role.PROPONENTE, "", Profile.ESTUDIANTE, "x@escuelaing.edu.co");
-        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user);
+        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user, Area.ENVIRONMENT);
         Upvote upvote = new Upvote(initiative, user);
         assertEquals(user, upvote.getUser());
     }
@@ -52,16 +51,17 @@ class UpvoteTest {
     @Test
     void shouldSetInitiative() {
         User user = new User("Equis", "abc", Role.PROPONENTE, "", Profile.ESTUDIANTE, "x@escuelaing.edu.co");
-        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user);
+        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user, Area.HEALTHY);
         Upvote upvote = new Upvote(initiative, user);
-        Initiative anotherInitiative = new Initiative("another project", State.REVIEW, "d", "e", "f", user);
+        Initiative anotherInitiative = new Initiative("another project", State.REVIEW, "d", "e", "f", user, Area.IA);
         upvote.setInitiative(anotherInitiative);
         assertEquals(anotherInitiative, upvote.getInitiative());
     }
+
     @Test
     void shouldGiveInitiative() {
         User user = new User("Equis", "abc", Role.PROPONENTE, "", Profile.ESTUDIANTE, "x@escuelaing.edu.co");
-        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user);
+        Initiative initiative = new Initiative("test idea", State.OPEN, "a", "b", "c", user, Area.UNDERTAKING);
         Upvote upvote = new Upvote(initiative, user);
         assertEquals(initiative, upvote.getInitiative());
     }
