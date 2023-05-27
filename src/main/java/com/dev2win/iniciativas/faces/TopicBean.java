211f70c9f7adadfd0edbf8af551c4bdf1a0791e1
ELS4NTA <daniel1sa_@hotmail.com>
2023-05-26 22:27:10 -0500

    Temas para las iniciativas, paginas y css

--- src/main/java/com/dev2win/iniciativas/faces/TopicBean.java
+++ src/main/java/com/dev2win/iniciativas/faces/TopicBean.java
@@ -3,6 +3,7 @@ package com.dev2win.iniciativas.faces;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,21 @@ public class TopicBean {
 
     @Autowired
     InitiativeService initiativeService;
-    private String topic = "";
-    private List<Initiative> selectedInitiatives = new ArrayList<>();
+
+    @Autowired
+    PrimeFacesWrapper primeFacesWrapper;
+
+    @Autowired
+    FacesContextWrapper facesContextWrapper;
+
+    private String topic;
+    private Topic selectedTopic;
+    private List<Topic> topics = new ArrayList<>();
+    private List<Initiative> initiatives;
+    private List<Initiative> selectedInitiatives;
+
+    private static final String TOPIC_MENU_MESSAGES = "topic-menu:messages";
+    private static final String TOPIC_MENU_TOPIC_LIST = "topic-menu:topic-list";
 
     /**
      * Empty constructor
@@ -32,29 +46,96 @@ public class TopicBean {
         // empty constructor
     }
 
-    public void createTopic(String name) {
-        topicService.addTopic(new Topic(name));
+    public String getTopic() {
+        return topic;
     }
 
-    public void deleteTopic(String topic) {
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Topic getSelectedTopic() {
+        return selectedTopic;
+    }
+
+    public void setSelectedTopic(Topic selectedTopic) {
+        this.selectedTopic = selectedTopic;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<Topic> topics) {
+        this.topics = topics;
+    }
+
+    public List<Initiative> getSelectedInitiatives() {
+        return selectedInitiatives;
+    }
+
+    public void setSelectedInitiatives(List<Initiative> selectedInitiatives) {
+        this.selectedInitiatives = selectedInitiatives;
+    }
+
+    public List<Initiative> getInitiatives() {
+        return initiatives;
+    }
+
+    public void setInitiatives(List<Initiative> initiatives) {
+        this.initiatives = initiatives;
+    }
+
+    public void newTopic() {
+        this.selectedTopic = new Topic();
+    }
+
+    public void saveTopic() {
+        topicService.addTopic(this.selectedTopic);
+        this.selectedTopic = null;
+        facesContextWrapper.getCurrentInstance().addMessage(null, new FacesMessage("Created topic"));
+        primeFacesWrapper.current().ajax().update(TOPIC_MENU_MESSAGES);
+    }
+
+    public void onDatabaseLoaded() {
+        this.initiatives = initiativeService.getAllInitiatives();
+        this.topics = topicService.getAllTopics();
+    }
+
+    public void deleteTopic() {
         Topic topicToDelete = topicService.getTopicByTopicName(topic);
-        List<Initiative> topicInitiatives = topicToDelete.getInitiatives();
-
-        for (Initiative i : topicInitiatives) {
-            i.setTopic(null);
+        List<Initiative> topicInitiatives = initiativeService.getInitiativesByTopic(topicToDelete.getTopicId());
+        for (Initiative initiative : topicInitiatives) {
+            initiative.setTopic(null);
+            initiativeService.updateInitiative(initiative);
         }
-
         topicService.deleteTopic(topicToDelete.getTopicId());
+        facesContextWrapper.getCurrentInstance().addMessage(null, new FacesMessage("Deleted topic"));
+        primeFacesWrapper.current().ajax().update(TOPIC_MENU_TOPIC_LIST, TOPIC_MENU_MESSAGES);
     }
 
     public void addInitiativeToTopic() {
+        Topic topicToAdd = topicService.getTopicByTopicName(topic);
         try {
-            for (Initiative i : selectedInitiatives) {
-                i.setTopic(topicService.getTopicByTopicName(topic));
+            for (Initiative initiative : selectedInitiatives) {
+                initiative.setTopic(topicToAdd);
+                initiativeService.updateInitiative(initiative);
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            facesContextWrapper.getCurrentInstance().addMessage(null, new FacesMessage("Grouped by selected topic"));
+            primeFacesWrapper.current().ajax().update(TOPIC_MENU_TOPIC_LIST, TOPIC_MENU_MESSAGES);
         }
+    }
+
+    public String getUpdateButtonMessage() {
+        String message = "Group";
+        if (hasSelectedInitiatives()) {
+            int size = this.selectedInitiatives.size();
+            return size > 1 ? message + " " + size + " initiatives selected" : message + " 1 initiative selected";
+        }
+        return message;
     }
 
     public boolean hasSelectedInitiatives() {
